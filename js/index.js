a8bb1646534917b63762286da005b5fae8111085
pjap <pranajaya.pratama@gmail.com>
2018-06-15 23:49:19 +0700

    Add Looping Array and Object Example

--- js/index.js
+++ js/index.js
@@ -1,6 +1,11 @@
 new Vue ({
   el : '#app',
   data : {
-    show : true
+    show : true,
+    ingredients : ['meat', 'fruit', 'cookies'],
+    persons : [
+      {name: 'Prana', age: 24, color: 'red'},
+      {name: 'Anna', age: 'unknown', color: 'blue'}
+    ]
   }
 })
